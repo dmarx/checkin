b74e23f7abdc245e99d5226095861a89ea128231
David Marx <david.marx84@gmail.com>
2020-10-16 22:33:23 -0700

    Added some caching to improve performance, simplified some processing

--- checkin/streamlit_app.py
+++ checkin/streamlit_app.py
@@ -12,68 +12,48 @@ import plotly.graph_objs as go
 
 db = SessionLocal()
 
-#@st.cache
 def get_data(db):
-    df_data_ref = get_checkins_df(db)
-    #df_data = df_data_ref
-    df_data = pd.DataFrame(df_data_ref)
-    #df_data.columns = df_data_ref.columns
-
+    df_data = get_checkins_df(db)
     idx_valued = df_data['value'].notnull()
     idx_text = df_data['comments'].notnull()
-
     df_text   = df_data[idx_text]
     df_scalar = df_data[idx_valued]
-
-    df_scalar_wk = (df_scalar
-                        .groupby([df_scalar['timestamp'].dt.week, 'parent', 'event_type'])
-                        ['value']
-
-                        .agg(['min', 'max', 'sum', 'count'])
-                        .reset_index()
-                        .rename(columns={'timestamp':'week'})
-                   )
-    return df_data, df_scalar, df_scalar_wk, df_text
-    #return df_data
-
+    return df_data.copy(), df_scalar.copy(), df_text.copy()
 
 @st.cache
-def process_data(#df, 
+def process_data(df, 
                  dt_start=None,
                  dt_end=None
                 ):
     aggs = ['min', 'max', 'sum', 'count']
-    df = df_scalar.copy()
-    #print(dt_start, dt_end)
     if dt_start is None:
         dt_start = df['timestamp'].min()
     if dt_end is None:
         dt_end = df['timestamp'].max()
-    #print(dt_start, dt_end)
-    #df = df[dt_start <= df['timestamp'] <= dt_end]
     df = df[dt_start <= df['timestamp']]
     df = df[df['timestamp'] <= dt_end]
     dt_agg= df['timestamp'].dt.week
     grpd = df.groupby([dt_agg, 'parent', 'event_type'])
     outv = grpd['value'].agg(aggs).reset_index().rename(columns={'timestamp':'week'})
-    #print(outv['week'].min(), outv['week'].max())
-    #print("data processed")
-    return outv.to_json(None)
+    return outv
     
-def update_figure(json_df,
-                  parent='Chores', 
+@st.cache
+def filter_parent(df, parent='Chores'):
+    #df_sub1[df_sub1['parent'] == parent]
+    return df[df['parent'] == parent]
+    
+def update_figure(df,
                   agg=['min', 'max', 'sum', 'count'][0], 
                   ctype='Stacked', 
                   flipxy=False, 
+                  parent='Chores'
                   ):
-    df_sub1 = pd.read_json(json_df)
-    df_sub1 = df_sub1[df_sub1['parent'] == parent]
     traces = []
     xcol = 'week'
     grpcol = 'event_type'
     if flipxy:
         xcol, grpcol = grpcol, xcol
-    for event_type, subset in df_sub1.groupby(grpcol):
+    for event_type, subset in df.groupby(grpcol):
         trace = go.Bar(x=subset[xcol],
                        y=subset[agg],
                        name=event_type
@@ -81,8 +61,7 @@ def update_figure(json_df,
         traces.append(trace)
     layout = go.Layout(hovermode='closest', title=parent)
     fig = go.Figure(data=traces, layout=layout)
-    if ctype == 'Stacked':
-        fig.update_layout(barmode='stack')        
+    fig.update_layout(barmode=ctype)        
     return fig
 
 if __name__ == '__main__':
@@ -91,8 +70,11 @@ if __name__ == '__main__':
     # Processing #
     ##############
 
-    df_data, df_scalar, df_scalar_wk, df_text = get_data(db)
-    d = process_data()
+    #df_data, df_scalar , df_scalar_wk, df_text = get_data(db)
+    df_data, df_scalar, df_text = get_data(db)
+    #df_data, df_scalar, df_text = get_data()
+    df = process_data(df_scalar.copy())
+    df = filter_parent(df)
     
     ######################
     # Controls #
@@ -101,12 +83,16 @@ if __name__ == '__main__':
     flipxy = False
     if st.checkbox('Event/Time on X-Axis'):
         flipxy = True
-    
-    fig = update_figure(d, flipxy=flipxy) 
-    
-    fig.update_layout(barmode='group')
+        
+    ctype='group'
     if st.checkbox('Grouped/Stacked'):
-        fig.update_layout(barmode='stack')  
+        ctype='stack'
+    
+    fig = update_figure(df, flipxy=flipxy, ctype=ctype) 
+    
+    #fig.update_layout(barmode='group')
+    #if st.checkbox('Grouped/Stacked'):
+    #    fig.update_layout(barmode='stack')  
     
     ######################
     # Layout #
